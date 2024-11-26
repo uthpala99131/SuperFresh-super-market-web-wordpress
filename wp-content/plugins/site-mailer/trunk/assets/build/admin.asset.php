31b7ebcd901ae4169ef789b81e5f650192bbd133
uthpala abeysinghe <162332672+uthpala99131@users.noreply.github.com>
2024-11-26 19:23:08 +0530

    update

---
+++ wp-content/plugins/site-mailer/trunk/assets/build/admin.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '4e86b01d103480c0a12a');
---
+++ wp-content/plugins/site-mailer/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'elementor/site-mailer',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'a0c8f51652a02d7efe067a7c6b885c25f3387d94',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'elementor/site-mailer' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'a0c8f51652a02d7efe067a7c6b885c25f3387d94',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v6.10.0',
+            'version' => '6.10.0.0',
+            'reference' => 'a49db6f0a5033aef5143295342f1c95521b075ff',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
